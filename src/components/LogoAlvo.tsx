1d92841410143478b6bef0ada8ee022f65fccdd8
marceliomp <marcelio.aguiac@gmail.com>
2025-10-28 01:37:07 -0300

    Replace binary logo with inline SVG component

---
+++ src/components/LogoAlvo.tsx
@@ -0,0 +1,36 @@
+export default function LogoAlvo() {
+  return (
+    <svg
+      width="56"
+      height="56"
+      viewBox="0 0 56 56"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-labelledby="logoAlvoTitle logoAlvoDesc"
+    >
+      <title id="logoAlvoTitle">Alvo BR</title>
+      <desc id="logoAlvoDesc">Marca simplificada da Alvo BR</desc>
+      <circle cx="28" cy="28" r="28" fill="#0D3B4C" />
+      <path
+        d="M28 11C20.8203 11 15 16.8203 15 24C15 31.1797 20.8203 37 28 37C35.1797 37 41 31.1797 41 24C41 16.8203 35.1797 11 28 11ZM28 15.5C32.1421 15.5 35.5 18.8579 35.5 23C35.5 27.1421 32.1421 30.5 28 30.5C23.8579 30.5 20.5 27.1421 20.5 23C20.5 18.8579 23.8579 15.5 28 15.5Z"
+        fill="#00A99D"
+      />
+      <path
+        d="M28 21C26.8954 21 26 21.8954 26 23C26 24.1046 26.8954 25 28 25C29.1046 25 30 24.1046 30 23C30 21.8954 29.1046 21 28 21Z"
+        fill="white"
+      />
+      <text
+        x="28"
+        y="45"
+        textAnchor="middle"
+        fontFamily="Montserrat, Arial, sans-serif"
+        fontSize="9"
+        fontWeight="700"
+        fill="#FFFFFF"
+      >
+        ALVO BR
+      </text>
+    </svg>
+  );
+}
